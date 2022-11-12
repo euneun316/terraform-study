39da4c3f372127f3e35744b3e8441153f9b127ec
ImOk <eunok169@gmail.com>
2022-11-13 05:01:44 +0900

    add learn-terraform modules

---
+++ T101/WEEK4/learn-terraform-modules-use/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "ap-northeast-2"
+
+  default_tags {
+    tags = {
+      hashicorp-learn = "module-use"
+    }
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "3.5.0"
+  count   = 2
+
+  name = "my-ec2-cluster"
+
+  ami                    = "ami-09cf633fe86e51bf0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
